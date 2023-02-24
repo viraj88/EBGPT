0cdcd2f6093c77cf9e3d33d658b4708ef2503229
Kulkarni <extern.VirajV.Kulkarni@elektrobit.com>
2023-02-24 11:50:24 +0530

    initial commit

---
+++ client/src/OpenAISVGLogo.js
@@ -0,0 +1,14 @@
+// OpenAI SVG Logo
+
+const OpenAISVGLogo = () => (
+  <div class="w-1">
+    <div style={{ width: "80px", height: "80px" }}>
+      <img align = "left"
+        src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/v1420630494/ymszwf9w5szws2omvsua.png"
+        style={{ width: "100%", height: "100%", borderRadius: "50%" }}
+      ></img>
+    </div>
+  </div>
+);
+
+export default OpenAISVGLogo;
